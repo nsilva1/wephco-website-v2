9090aa6e4f3337c524bfd6eb06fe48630eeb2524
Neto <ukpongneto@wellahealth.com>
2025-05-06 01:08:29 +0100

    careers page completed

---
+++ app/careers/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const CareersPage = () => {
+  return (
+    <div>
+      <div className='flex flex-col items-center justify-center min-h-screen'>
+        <h1 className='text-3xl font-bold text-center mt-10'>Careers</h1>
+        <p className='text-center mt-4'>No open positions at the moment!</p>
+        {/* <div className='flex justify-center mt-10'>
+            <a
+            href='/careers/apply'
+            className='bg-black text-white px-4 py-2 rounded'
+            >
+            Apply Now
+            </a>
+        </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default CareersPage;
