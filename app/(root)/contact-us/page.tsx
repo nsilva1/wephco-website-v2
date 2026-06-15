ead42cdff6a3dfa60456b6559ba682067f40e922
Neto <ukpongneto@gmail.com>
2026-06-15 17:38:17 +0100

    contact us page update completed

--- app/(root)/contact-us/page.tsx
+++ app/(root)/contact-us/page.tsx
@@ -5,7 +5,6 @@ const ContactPage = () => {
   return (
     <div>
       <div className="flex flex-col items-center justify-center min-h-screen py-2 font-outfit">
-        <p className="text-lg mb-8 mt-20">We'd love to hear from you!</p>
         <ContactUs />
       </div>
     </div>
