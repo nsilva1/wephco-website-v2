8fd51a70cbdc0dd9c23a72c6bbe5ee3e6b257390
Wephco <94122928+Wephco@users.noreply.github.com>
2026-08-19 23:16:07 +0100

    Update layout.tsx
    added google analytics tag script

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-S4KJ9R4BVF"
+          strategy="afterInteractive"
+        />
+        <Script id="google-tag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-S4KJ9R4BVF');
+          `}
+        </Script>
+        <Script
+          src="https://t.contentsquare.net/uxa/65358b2c8c4e3.js"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className={`${outfit.variable} antialiased`}>{children}</body>
     </html>
   );
